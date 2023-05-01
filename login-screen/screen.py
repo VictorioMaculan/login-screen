62d16c5d76360a29ec5d21a4d8acde0eab941ade
Victório Maculan <123396267+MestreDosPATUS@users.noreply.github.com>
2023-05-01 11:47:04 -0300

    Created the logged scrren
    Created the logged screen and made a few other changes.

--- login-screen/screen.py
+++ login-screen/screen.py
@@ -1,38 +1,42 @@
 import tkinter as tk
 import sqlite3
+
 defaultfont = ('Times New Roman', 12)
 database = 'users.db'
 
 class Gui:
     def __init__(self):   
         self.root = tk.Tk()
+        self.outmsg = tk.StringVar()
         self.activeframe = None
         
         self.registering()
+        self.outmsg.set('-')
         self.root.title('Login/Register')
         self.root.geometry('400x500')
         self.root.mainloop()
     
     @staticmethod
     def isRegisterValid(name, email, password):
+        if name.strip() == '' or email.strip() == '' or password.strip() == '':
+            return False
         with sqlite3.connect(database) as con:
             cursor = con.cursor()
             cursor.execute(f'select * from users where email = ?', (email,))
             out = cursor.fetchall()
             cursor.close()
-        return (len(out) == 0) and (name.strip() != password.strip() != '')
+        return (len(out) == 0)
 
     @staticmethod
     def isLoginValid(email, password):
         with sqlite3.connect(database) as con:
             cursor = con.cursor()
-            cursor.execute(f'select * from users where email = ? and password = ?', (email, password))
+            cursor.execute('select * from users where email = ? and password = ?', (email, password))
             out = cursor.fetchall()
             cursor.close()
         return len(out) > 0        
     
-    @staticmethod
-    def newuser(name, email, password):
+    def newuser(self, name, email, password):
         with sqlite3.connect(database) as con:
             cursor = con.cursor()
             try:
@@ -47,6 +51,8 @@ class Gui:
                             values(?, ?, ?)''', (name, email, password))
                 con.commit()
                 cursor.close()
+        self.outmsg.set('Cadastrado. Por favor, fazer login.')
+        self.login()
                 
     def activate(self, frame):
         if self.activeframe is not None:
@@ -71,8 +77,10 @@ class Gui:
         password = tk.Entry(register, font=defaultfont, width=35)
         password.pack()
 
-        tk.Button(register, text='Registrar', font=defaultfont).pack(pady=10)
+        tk.Button(register, text='Registrar', font=defaultfont, 
+                  command=lambda: self.finalver(name.get(), email.get(), password.get())).pack(pady=10)
 
+        tk.Label(register, textvariable=self.outmsg, font=defaultfont).pack(side='bottom', pady=20)
         tk.Button(register, text='Login', font=('Times New Roman', 10), command=self.login).pack(side='bottom')
         tk.Label(register, text='Ja tem uma conta?', font=('Times New Roman', 10)).pack(side='bottom')
         self.activate(register)
@@ -90,14 +98,21 @@ class Gui:
         password = tk.Entry(login_, font=defaultfont, width=35)
         password.pack()
         
-        tk.Button(login_, text='Login', font=defaultfont, command=self.emailverification).pack(pady=10)
+        tk.Button(login_, text='Login', font=defaultfont).pack(pady=10)
         
+        tk.Label(login_, textvariable=self.outmsg, font=defaultfont).pack(side='bottom', pady=20)
         tk.Button(login_, text='Cadastrar', font=('Times New Roman', 10), command=self.registering).pack(side='bottom')
         tk.Label(login_, text='Não tem uma conta?', font=('Times New Roman', 10)).pack(side='bottom')
         self.activate(login_)
         
-    def emailverification(self, email):
-        sendcode = '123'
+    def finalver(self, name, email, password):
+        from random import randint
+        
+        if not self.isRegisterValid(name, email, password):
+            self.outmsg.set(('[ERRO: Informação(s) inválida(s)].'))
+            return
+
+        sendcode = str(randint(100, 999))
         verify = tk.Frame(self.root)
         
         tk.Label(verify, text='Verificação do Email', font=('Times New Roman', 16)).pack(pady=20)
@@ -107,8 +122,20 @@ class Gui:
         code = tk.Entry(verify, font=('Times New Roman', 22), width=3)
         code.pack()
         
-        tk.Button(verify, text='Continuar', font=defaultfont).pack(pady=10)
-        self.activate(verify)     
+        compare = lambda: self.newuser(name, email, password) if code.get() == sendcode else self.outmsg.set('Código Inválido')
+        tk.Button(verify, text='Continuar', font=defaultfont, command=compare).pack(pady=10)
         
-        # TODO: Email verification and validate/create login/registering.  
+        tk.Label(verify, textvariable=self.outmsg, font=defaultfont).pack(side='bottom', pady=20)
+        self.activate(verify)
+
+    def loggedscreen(self, name):
+        
+        # TODO: Adicionar uma verificação de login válido, subir erro se não for.
+        
+        logged = tk.Frame(self.root)
+        tk.Label(logged, text=f'Olá {name}, agora você está logado(a)!', font=defaultfont).pack()
+        tk.Button(logged, text='Voltar', font=defaultfont, command=self.login).pack()
+        
+        self.activate(logged)
+
 Gui()
