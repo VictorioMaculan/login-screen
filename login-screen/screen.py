c05be7f3643e5332773358bb7f2d9170bdbef856
Victório Maculan <123396267+MestreDosPATUS@users.noreply.github.com>
2023-05-06 22:14:13 -0300

    Fixing and Basic Fast Login
    Fixed somethings and made the basic Fast Login System

--- login-screen/screen.py
+++ login-screen/screen.py
@@ -3,17 +3,17 @@ import sqlite3
 
 defaultfont = ('Times New Roman', 12)
 database = 'C:\\Users\\User\\Documents\\GitHub\\login-screen\\login-screen\\users.db'
-message_file = 'C:\\Users\\User\\Documents\\GitHub\\login-screen\\login-screen\\msg.txt'
-systememail = '-'
 
 class Gui:
     def __init__(self):   
+        self.preparedb()
+        
         self.root = tk.Tk()
         self.outmsg = tk.StringVar()
+        self.fastusers = self.getSavedUsers()
         self.activeframe = None
         
-        self.registering()
-        self.preparedb()
+        self.choose()
         
         self.outmsg.set('-')
         self.root.title('Login/Register')
@@ -23,7 +23,7 @@ class Gui:
     
     @staticmethod
     def isRegisterValid(name, email, password):
-        if name.strip() == '' or email.strip() == '' or password.strip() == '':
+        if name.strip() == '' or password.strip() == '':
             return False
         with sqlite3.connect(database) as con:
             cursor = con.cursor()
@@ -39,39 +39,61 @@ class Gui:
             cursor.execute('select * from users where email = ? and password = ?', (email, password))
             out = cursor.fetchall()
             cursor.close()
-        return len(out) > 0        
+        return len(out) > 0
     
     @staticmethod
-    def sendemail(email, sendcode='000'):
-        import smtplib
-        from email.message import EmailMessage
-        
-        message = EmailMessage()
-        with open(message_file) as msg:
-            message.set_content(msg.read().replace('{sendcode}', str(sendcode)))
-        
-        message['Subject'] = 'Registro (Login/Registering System)'
-        message['From'] = systememail
-        message['To'] = email
-        
-        with smtplib.SMTP(host='localhost') as host:
-            host.send_message(message)
-        # TODO: Finish and fix this.    
-        
-            
+    def getSavedUsers():
+        with sqlite3.connect(database) as con:
+            con.row_factory = sqlite3.Row
+            cursor = con.cursor()
+            out = [register for register in cursor.execute('select * from fastusers')]
+            cursor.close()
+        return out
+    
     @staticmethod
     def preparedb():
         with sqlite3.connect(database) as con:
             cursor = con.cursor()
-        try:
-            cursor.execute('''create table users(id integer primary key autoincrement,
-                name text,
-                email text,
-                password text)''')
-        except sqlite3.OperationalError:
+            try:
+                cursor.execute('''create table users(id integer primary key autoincrement,
+                    name text,
+                    email text,
+                    password text)''')
+                cursor.execute('''create table fastusers(id integer primary key autoincrement,
+                               name text,
+                               email text,
+                               password text)''')
+            except sqlite3.OperationalError:
+                return
+            finally:
+                cursor.close()
+                con.commit()
+                    
+    def activate(self, frame):
+        if self.activeframe is not None:
+            self.activeframe.destroy()
+        self.activeframe = frame
+        self.activeframe.pack()
+        self.outmsg.set('-')    
+        
+    def newFastUser(self, name, email, password):
+        if len(self.fastusers) <= 3:
+            self.outmsg.set('Você já atingiu o limite de Logins Rápidos.')
             return
         
+        with sqlite3.connect(database) as con:
+            cursor = con.cursor()
+            cursor.execute('''insert into fastusers(name, email, password)
+                           values(?, ?, ?, ?)''', (name, email, password))
+            cursor.close()
+            con.commit()
+        self.outmsg.set('Adicionado aos Logins Rápidos.')
+            
     def newuser(self, name, email, password):
+        if not self.isRegisterValid(name, email, password):
+            self.outmsg.set('[ERRO: Parâmetro(s) Inválido(s)/Email já em Uso]')
+            return
+        
         with sqlite3.connect(database) as con:
             cursor = con.cursor()
             cursor.execute('''insert into users(name, email, password)
@@ -80,12 +102,6 @@ class Gui:
             con.commit()
         self.outmsg.set('Cadastrado. Por favor, fazer login.')
         self.login()
-                
-    def activate(self, frame):
-        if self.activeframe is not None:
-            self.activeframe.destroy()
-        self.activeframe = frame
-        self.activeframe.pack()
         
     def registering(self):
         register = tk.Frame(self.root)
@@ -105,7 +121,9 @@ class Gui:
         password.pack()
 
         tk.Button(register, text='Registrar', font=defaultfont, 
-                  command=lambda: self.finalver(name.get(), email.get(), password.get())).pack(pady=10)
+                  command=lambda: self.newuser(name.get().strip(), 
+                                               email.get().strip(), 
+                                               password.get().strip())).pack(pady=10)
 
         tk.Label(register, textvariable=self.outmsg, font=defaultfont).pack(side='bottom', pady=20)
         tk.Button(register, text='Login', font=('Times New Roman', 10), command=self.login).pack(side='bottom')
@@ -125,48 +143,52 @@ class Gui:
         password = tk.Entry(login_, font=defaultfont, width=35)
         password.pack()
         
-        tk.Button(login_, text='Login', font=defaultfont, command=lambda: self.loggedscreen(email.get(), password.get())).pack(pady=10)
+        tk.Button(login_, text='Login', font=defaultfont, command=lambda: self.logged(
+            email.get().strip(), password.get().strip(),)).pack(pady=10)
+        
+        tk.Checkbutton()
         
         tk.Label(login_, textvariable=self.outmsg, font=defaultfont).pack(side='bottom', pady=20)
         tk.Button(login_, text='Cadastrar', font=('Times New Roman', 10), command=self.registering).pack(side='bottom')
         tk.Label(login_, text='Não tem uma conta?', font=('Times New Roman', 10)).pack(side='bottom')
         self.activate(login_)
         
-    def finalver(self, name, email, password):
-        from random import randint
-        
-        if not self.isRegisterValid(name, email, password):
-            self.outmsg.set(('[ERRO: Parâmetro(s) inválido(s)].'))
-            return
-
-        sendcode = str(randint(100, 999))
-        self.outmsg.set(sendcode)
-        # self.sendemail(email, sendcode)
-        
-        verify = tk.Frame(self.root)
-        
-        tk.Label(verify, text='Verificação do Email', font=('Times New Roman', 16)).pack(pady=20)
-        tk.Label(verify, text=f'Um código será enviado a {email}', font=defaultfont).pack(pady=10)
-        tk.Label(verify, text='Insira o código de três digitos abaixo:').pack(pady=10)
-        
-        code = tk.Entry(verify, font=('Times New Roman', 22), width=3)
-        code.pack()
-        
-        compare = lambda: self.newuser(name, email, password) if code.get() == sendcode else self.outmsg.set('Código Inválido')
-        tk.Button(verify, text='Continuar', font=defaultfont, command=compare).pack(pady=10)
-        
-        tk.Label(verify, textvariable=self.outmsg, font=defaultfont).pack(side='bottom', pady=20)
-        self.activate(verify)
-
-    def loggedscreen(self, email, password):
+    def logged(self, email, password):
         if not self.isLoginValid(email, password):
             self.outmsg.set('[ERRO: Parâmetro(s) Inválido(s)/Senha Incorreta]. ')
             return
         
-        logged = tk.Frame(self.root)
-        tk.Label(logged, text=f'Olá {email}, agora você está logado(a)!', font=defaultfont).pack(pady=20)
+        with sqlite3.connect(database) as con:
+            con.row_factory = sqlite3.Row
+            cursor = con.cursor()
+            account = [x for x in cursor.execute('select * from users where email = ?', (email,))][0]
+            
         
-        tk.Button(logged, text='Sair', font=defaultfont, command=self.login).pack(pady=20)
-        self.activate(logged)
+        logged_ = tk.Frame(self.root, width=500, height=400)
+        tk.Label(logged_, text=f'Logado! Informações da Conta:', font=('Times New Roman', 16)).place(y=12, x=200, anchor='center')
+        
+        tk.Label(logged_, text=f'Nome = {account["name"]}', font=defaultfont).place(y=30, x=0)
+        tk.Label(logged_, text=f'Email = {account["email"]}', font=defaultfont).place(y=60, x=0)
+        tk.Label(logged_, text=f'Senha = {account["password"]}', font=defaultfont).place(y=90, x=0)
 
+        tk.Button(logged_, text='Ativar Login Rápido', font=defaultfont,
+                  command=lambda: self.newFastUser(account['name'],
+                                                   account['email'],
+                                                   account['password'])).place(y=125, x=5)
+        
+        tk.Button(logged_, text='Sair', font=defaultfont, command=self.login).place(y=165, x=5)
+        
+        tk.Label(logged_, textvariable=self.outmsg, font=defaultfont).place(y=215, x=200, anchor='center')
+        self.activate(logged_)
+        
+    def choose(self):
+        choose_ = tk.Frame(self.root, width=500, height=400)
+        tk.Label(choose_, text='Você Tem Logins Rápidos Salvos', 
+                 font=('Times New Roman', 16)).place(y=12, x=200, anchor='center')
+
+        # TODO: Mostrar contas disponíveis para Fast-Login
+        
+        tk.Label()
+
+        self.activate(choose_)
 Gui()
