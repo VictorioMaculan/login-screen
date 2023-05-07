f0e8b132925b0d8d51fb74ad1264ca069518657c
Victório Maculan <123396267+MestreDosPATUS@users.noreply.github.com>
2023-05-07 19:10:20 -0300

    Publish V.1

--- login-screen/screen.py
+++ login-screen/screen.py
@@ -10,46 +10,20 @@ class Gui:
         
         self.root = tk.Tk()
         self.outmsg = tk.StringVar()
-        self.fastusers = self.getSavedUsers()
+        self.fastusers = self.getTableContent(table='fastusers')
         self.activeframe = None
         
-        self.choose()
+        if len(self.fastusers) == 0:
+            self.registering()
+        else:
+            self.choose()
         
         self.outmsg.set('-')
         self.root.title('Login/Register')
         self.root.geometry('400x500')
         
         self.root.mainloop()
-    
-    @staticmethod
-    def isRegisterValid(name, email, password):
-        if name.strip() == '' or password.strip() == '':
-            return False
-        with sqlite3.connect(database) as con:
-            cursor = con.cursor()
-            cursor.execute(f'select * from users where email = ?', (email,))
-            out = cursor.fetchall()
-            cursor.close()
-        return (len(out) == 0)
-
-    @staticmethod
-    def isLoginValid(email, password):
-        with sqlite3.connect(database) as con:
-            cursor = con.cursor()
-            cursor.execute('select * from users where email = ? and password = ?', (email, password))
-            out = cursor.fetchall()
-            cursor.close()
-        return len(out) > 0
-    
-    @staticmethod
-    def getSavedUsers():
-        with sqlite3.connect(database) as con:
-            con.row_factory = sqlite3.Row
-            cursor = con.cursor()
-            out = [register for register in cursor.execute('select * from fastusers')]
-            cursor.close()
-        return out
-    
+        
     @staticmethod
     def preparedb():
         with sqlite3.connect(database) as con:
@@ -68,28 +42,70 @@ class Gui:
             finally:
                 cursor.close()
                 con.commit()
+                
+    @staticmethod
+    def getTableContent(table):
+        with sqlite3.connect(database) as con:
+            con.row_factory = sqlite3.Row
+            cursor = con.cursor()
+            out = [register for register in cursor.execute(f'select * from {table}')]
+            cursor.close()
+        return out if out is not None else list()
+    
+    @staticmethod
+    def isEmailUnique(email, table):
+        with sqlite3.connect(database) as con:
+            cursor = con.cursor()
+            cursor.execute(f'select * from {table} where email = ?', (email,))
+            out = cursor.fetchall()
+            cursor.close()
+        return (len(out) == 0)
+        
+    @staticmethod
+    def isLoginValid(email, password):
+        with sqlite3.connect(database) as con:
+            cursor = con.cursor()
+            cursor.execute('select * from users where email = ? and password = ?', (email, password))
+            out = cursor.fetchall()
+            cursor.close()
+        return len(out) > 0
+    
+    def isRegisterValid(self, name, email, password):
+        if name.strip() == '' or password.strip() == '' or not self.isEmailUnique(email, table='users'):
+            return False
+        return True
                     
     def activate(self, frame):
         if self.activeframe is not None:
             self.activeframe.destroy()
         self.activeframe = frame
         self.activeframe.pack()
-        self.outmsg.set('-')    
         
     def newFastUser(self, name, email, password):
-        if len(self.fastusers) <= 3:
+        if len(self.fastusers) >= 3:
             self.outmsg.set('Você já atingiu o limite de Logins Rápidos.')
+            return
+        if not self.isEmailUnique(email, table='fastusers'):
+            self.outmsg.set('Email já registrado nos Logins Rápidos.')
             return
         
         with sqlite3.connect(database) as con:
             cursor = con.cursor()
             cursor.execute('''insert into fastusers(name, email, password)
-                           values(?, ?, ?, ?)''', (name, email, password))
+                           values(?, ?, ?)''', (name, email, password))
             cursor.close()
             con.commit()
         self.outmsg.set('Adicionado aos Logins Rápidos.')
-            
-    def newuser(self, name, email, password):
+    
+    def delFastUser(self, email):
+        with sqlite3.connect(database) as con:
+            cursor = con.cursor()
+            cursor.execute(f'delete from fastusers where email = ?', (email,))
+            cursor.close()
+        self.fastusers = self.getTableContent(table='fastusers')
+        self.choose()
+    
+    def newUser(self, name, email, password):
         if not self.isRegisterValid(name, email, password):
             self.outmsg.set('[ERRO: Parâmetro(s) Inválido(s)/Email já em Uso]')
             return
@@ -121,7 +137,7 @@ class Gui:
         password.pack()
 
         tk.Button(register, text='Registrar', font=defaultfont, 
-                  command=lambda: self.newuser(name.get().strip(), 
+                  command=lambda: self.newUser(name.get().strip(), 
                                                email.get().strip(), 
                                                password.get().strip())).pack(pady=10)
 
@@ -145,8 +161,6 @@ class Gui:
         
         tk.Button(login_, text='Login', font=defaultfont, command=lambda: self.logged(
             email.get().strip(), password.get().strip(),)).pack(pady=10)
-        
-        tk.Checkbutton()
         
         tk.Label(login_, textvariable=self.outmsg, font=defaultfont).pack(side='bottom', pady=20)
         tk.Button(login_, text='Cadastrar', font=('Times New Roman', 10), command=self.registering).pack(side='bottom')
@@ -186,9 +200,49 @@ class Gui:
         tk.Label(choose_, text='Você Tem Logins Rápidos Salvos', 
                  font=('Times New Roman', 16)).place(y=12, x=200, anchor='center')
 
-        # TODO: Mostrar contas disponíveis para Fast-Login
+
+        if len(self.fastusers) >= 1:
+            account1 = self.fastusers[0]
         
-        tk.Label()
+            tk.Label(choose_, text=f'> {account1["name"]} ({account1["email"]})',
+                     font=('Times New Roman', 14)).place(x=5, y=50)
+            tk.Button(choose_, text='Logar', font=defaultfont, 
+                      command=lambda: self.logged(account1['email'], account1['password'])).place(x=5, y=80)
+            tk.Button(choose_, text='Remover de Logins Rápidos', font=defaultfont,
+                      command=lambda: self.delFastUser(account1['email'])).place(x=60, y=80)
+        else:
+            tk.Label(choose_, text='> Conta 1 (Vazio)', font=('Times New Roman', 14)).place(x=5, y=50)
+            
+            
+        if len(self.fastusers) >= 2:
+            account2 = self.fastusers[1]
+            
+            tk.Label(choose_, text=f'> {account2["name"]} ({account2["email"]})',
+                     font=('Times New Roman', 14)).place(x=5, y=150)
+            tk.Button(choose_, text='Logar', font=defaultfont, 
+                      command=lambda: self.logged(account2['email'], account2['password'])).place(x=5, y=180)
+            tk.Button(choose_, text='Remover de Logins Rápidos', font=defaultfont,
+                      command=lambda: self.delFastUser(account2['email'])).place(x=60, y=180)
+        else:
+            tk.Label(choose_, text='> Conta 2 (Vazio)', font=('Times New Roman', 14)).place(x=5, y=150)
+        
+        
+        if len(self.fastusers) >= 3:
+            account3 = self.fastusers[2]
+            
+            tk.Label(choose_, text=f'> {account3["name"]} ({account3["email"]})',
+                     font=('Times New Roman', 14)).place(x=5, y=250)
+            tk.Button(choose_, text='Logar', font=defaultfont, 
+                      command=lambda: self.logged(account3['email'], account3['password'])).place(x=5, y=280)
+            tk.Button(choose_, text='Remover de Logins Rápidos', font=defaultfont,
+                      command=lambda: self.delFastUser(account3['email'])).place(x=60, y=280)
+        else:
+            tk.Label(choose_, text='> Conta 3 (Vazio)', font=('Times New Roman', 14)).place(x=5, y=250)    
+        
+        
+        tk.Label(choose_, text='Quer acessar outra conta?', font=('Times New Roman', 9)).place(y=330, x=200, anchor='center')
+        tk.Button(choose_, text='Login', font=('Times New Roman', 9), command=self.login).place(y=350, x=200, anchor='center')
 
         self.activate(choose_)
 Gui()
+# Todo: Acabar
